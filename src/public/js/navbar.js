e4566e729bfd72156995915a3e8707c14c070407
hemantpraja <hemantpraja88@gmail.com>
2023-10-19 14:23:07 +0530

    ikjdnve

--- src/public/js/navbar.js
+++
@@ -1,8 +0,0 @@
-
-var normal_user_profile_referenece = document.getElementById("normal-user-profile-reference");
-normal_user_profile_referenece.addEventListener("click",()=>{
-    console.log("User Dashboard ");
-    window.location.href = "/user/user_dashboard";
-    // window.location.href = "/user/driver_dashboard";//temparary
-    // window.location.href = "/user/owner_dashboard";//temparary
-})
